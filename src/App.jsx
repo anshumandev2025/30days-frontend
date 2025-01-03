e45f096ffa405c02bc482ad9274efc9c4ca81a88
anshumandev2002 <anshumandev2002@gmail.com>
2025-01-03 23:11:11 +0530

    day3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Day1 from "./pages/Day1";
-import Day2 from "./pages/Day2";
+import Day3 from "./pages/Day3";
 
 const App = () => {
   return (
     <>
       <div>
-        {/* <Day1 />
-        <hr /> */}
-        <Day2 />
+        {/* <Day1 /> */}
+        {/* <Day2/> */}
+        <Day3 />
       </div>
     </>
   );
